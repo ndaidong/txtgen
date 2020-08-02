41f857660c5f8f128574bb98e2b63e455c8ea9ea
8times12 <8times12@gmail.com>
2020-08-02 20:49:50 +0900

    TypeScript declaration files
    Add a TypeScript declaration file and its path.

---
+++ src/types/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for txtgen 2.2.6
+// TypeScript Version: 3.7
+
+declare module "txtgen" {
+  export function sentence(): string;
+  export function paragraph(len?: number): string;
+  export function article(len?: number): string;
+  
+  export function addNouns(ls?: string[]): number;
+  export function addAdjectives(ls?: string[]): number;
+  export function addTemplates(ls?: string[]): number;
+
+  export function setNouns(ls?: string[]): number;
+  export function setAdjectives(ls?: string[]): number;
+  export function setTemplates(ls?: string[]): number;
+
+  export function getNouns(): string[];
+  export function getAdjectives(): string[];
+  export function getTemplates(): string[];
+}
